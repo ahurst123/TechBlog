c0970f5df3002c4950691100adc2a76a91aca094
ahurst123 <austinhurst123@gmail.com>
2024-08-26 10:29:42 -0700

    added server.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../models');
+
+// Homepage route
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('home', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// other routes...
+
+module.exports = router;
